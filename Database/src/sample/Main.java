bc327d091cfbba24ef144dfb406e72bf4b5a700a
unknown <balabaev00@yandex.ru>
2020-02-19 10:02:10 +0900

    add icon (Database Project)

--- Database/src/sample/Main.java
+++ Database/src/sample/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setResizable(false);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 
