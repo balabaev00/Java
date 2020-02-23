5b723890e665009819bd8c79d549e82a382214f0
balabaev00 <balabaev00@yandex.ru>
2020-02-24 00:10:13 +0900

    update 1.4

--- Database/src/sample/Controller.java
+++ Database/src/sample/Controller.java
@@ -87,7 +87,7 @@ public class Controller {
     private TableColumn<Person, String> db_middle_name;
 
     @FXML
-    private TableColumn<Person, Position> db_position;
+    private TableColumn<Person, String> db_position;
 
     @FXML
     private TableColumn<Person, Short> db_pay;
@@ -104,10 +104,8 @@ public class Controller {
 
     @FXML
     private void handleCloseButtonAction(MouseEvent event) {
-        if (event.getTarget() == btn_close) {
-            Stage stage = (Stage) btn_home.getScene().getWindow();
-            stage.close();
-        }
+        Stage stage = (Stage) btn_home.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
@@ -147,22 +145,140 @@ public class Controller {
         }
     }
 
+    /**Проверка всех полей для добавления сотрудника**/
+    private boolean checkAdd() {
+        boolean flag = false;
+        Position temp = (Position) choise_position.getValue();
+        if (field_second_name.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Введите фамилию!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_second_name.getText().length() < 2) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Длина фамилии слишком маленькая!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_first_name.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Введите имя!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_first_name.getText().length() < 2) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Длина имени слишком маленькая!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_middle_name.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Введите отчество!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_middle_name.getText().length() < 2) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Длина отчества слишком маленькая!");
+            alert.showAndWait();
+            flag = true;
+        } else if (temp == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Выберите должность сотрудника!");
+            alert.showAndWait();
+            flag = true;
+        } else if (field_salary.getText().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Введите зарплату сотрудника!");
+            alert.showAndWait();
+            flag = true;
+        } else if (Short.parseShort(field_salary.getText()) < 1) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Ошибка");
+            alert.setHeaderText(null);
+            alert.setContentText("Зарплата сотрудника должна быть > 0");
+            alert.showAndWait();
+            flag = true;
+        }
+        return flag;
+    }
+
+    private short generateId() {
+        short random = (short) (80 + Math.random()*1000);
+        for (int i = 0; i < usersData.size(); i++) {
+            if(usersData.get(i).getId()==random) {
+                i=0;
+                random = (short) (80 + Math.random()*1000);
+            }
+        }
+        return random;
+    }
     @FXML
+    /**Добавление сотрудника в список**/
     private void handleAddUserAction(MouseEvent event) {
-        if (event.getTarget() == btn_add_person) {
-            Person person = new Person(field_second_name.getText(),field_first_name.getText(),field_middle_name.getText(),Position.Junior, (short) 1,Short.parseShort(field_salary.getText()));
-            usersData.add(person);
+        if (checkAdd() == false) {
+            usersData.add(new Person(field_second_name.getText(), field_first_name.getText(), field_middle_name.getText(), Position.Junior, generateId(), Short.parseShort(field_salary.getText())));
+        }
+    }
+
+    @FXML
+    /**Удаление из списка по фамилии**/
+    private void handleDelUserAction(MouseEvent event) {
+        if (usersData.size() != 0) {
+            short count = 0;
+            String secondName = field_del_person.getText();
+            if (!secondName.equals("")) {
+                for (int i = 0; i < usersData.size(); i++) {
+                    if (usersData.get(i).getSecondName().equals(secondName)) {
+                        usersData.remove(i);
+                        count++;
+                        i--;
+                    }
+                }
+                if (count == 0) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Информация");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Сотрудник с данной фамилией не найден!");
+                    alert.showAndWait();
+                }
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Ошибка");
+                alert.setHeaderText(null);
+                alert.setContentText("Введите фамилию сотрудника!");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Предупреждение");
+            alert.setHeaderText(null);
+            alert.setContentText("Список сотрудников пуст!");
+            alert.showAndWait();
         }
     }
 
     private void initTableView() {
         /**Устанавливаем тип и значение которое должно хранится в колонке**/
-        db_second_name.setCellValueFactory(new PropertyValueFactory<Person,String>("secondName"));
-        db_first_name.setCellValueFactory(new PropertyValueFactory<Person,String>("firstName"));
-        db_middle_name.setCellValueFactory(new PropertyValueFactory<Person,String>("middleName"));
-        db_position.setCellValueFactory(new PropertyValueFactory<Person,Position>("position"));
-        db_pay.setCellValueFactory(new PropertyValueFactory<Person,Short>("pay"));
-        db_id.setCellValueFactory(new PropertyValueFactory<Person,Short>("id"));
+        db_second_name.setCellValueFactory(new PropertyValueFactory<Person, String>("secondName"));
+        db_first_name.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        db_middle_name.setCellValueFactory(new PropertyValueFactory<Person, String>("middleName"));
+        db_position.setCellValueFactory(new PropertyValueFactory<Person, String>("position"));
+        db_pay.setCellValueFactory(new PropertyValueFactory<Person, Short>("pay"));
+        db_id.setCellValueFactory(new PropertyValueFactory<Person, Short>("id"));
         /**Заполняем таблицу данными из коллекции UsersData**/
         table_database.setItems(usersData);
     }
@@ -170,7 +286,7 @@ public class Controller {
     @FXML
     void initialize() {
         /**Установка вариантов для Choise Box**/
-        choise_position.setItems(FXCollections.observableArrayList(Position.Junior, Position.Middle, Position.Senior, Position.Директор));
+        choise_position.setItems(FXCollections.observableArrayList(Position.Junior, Position.Middle, Position.Senior, Position.Director));
         initTableView();
     }
 }
--- Database/src/sample/Position.java
+++ Database/src/sample/Position.java
@@ -1,8 +1,17 @@
 package sample;
 
 public enum Position {
-        Junior,
-        Middle,
-        Senior,
-        Директор
+        Junior("Junior"),
+        Middle("Middle"),
+        Senior("Senior"),
+        Director("Директор");
+        private String position;
+
+        Position(String position) {
+                this.position=position;
+        }
+
+        public String getPosition() {
+                return position;
+        }
 }
